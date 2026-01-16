7feb4a1627eb8c031fbb2cfcf4f68cea7ba429a3
Tiago Barbosa <tiagobbs17@gmail.com>
2026-01-16 16:47:34 +0000

    deixe o nome do titulo toda branco

--- src/components/landing/hero.tsx
+++ src/components/landing/hero.tsx
@@ -27,7 +27,7 @@ export function Hero() {
         <div className="flex flex-col items-center text-center space-y-6">
           <div className="flex flex-col justify-center space-y-4 text-white">
             <h1 className="font-headline text-4xl font-bold tracking-tighter sm:text-5xl md:text-6xl lg:text-7xl">
-              Conviértete en experta en <span className="text-primary">Mini Donas Caseras</span>
+              Conviértete en experta en Mini Donas Caseras
             </h1>
             <p className="max-w-[700px] mx-auto text-lg md:text-xl">
               Aprende <span className="text-primary font-bold">en menos de 7 días</span> las recetas más deliciosas y rentables de mini donas caseras y comienza <span className="text-primary font-bold">tu propio negocio rentable desde casa</span>. ¡Perfecto para ganar dinero extra!
