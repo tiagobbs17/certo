30a1063ced71a3f921c66533925138dc902a39cf
Tiago Barbosa <tiagobbs17@gmail.com>
2026-01-20 22:44:39 +0000

    troque o video por esse <iframe id="panda-456c288d-4d2c-4239-b12b-6e55d7

--- src/components/landing/hero.tsx
+++ src/components/landing/hero.tsx
@@ -30,7 +30,7 @@ export function Hero() {
           </div>
           
           <div className="w-full max-w-2xl overflow-hidden shadow-2xl relative pb-[56.25%]">
-            <iframe id="panda-8864ef1f-1e0f-4884-9fbd-e6c50dd2de1b" src="https://player-vz-208f6759-5ea.tv.pandavideo.com.br/embed/?v=8864ef1f-1e0f-4884-9fbd-e6c50dd2de1b" style={{border: 'none'}} allow="accelerometer;gyroscope;autoplay;encrypted-media;picture-in-picture" allowFullScreen={true} className="absolute top-0 left-0 w-full h-full" fetchPriority="high"></iframe>
+            <iframe id="panda-456c288d-4d2c-4239-b12b-6e55d7b2bca9" src="https://player-vz-208f6759-5ea.tv.pandavideo.com.br/embed/?v=456c288d-4d2c-4239-b12b-6e55d7b2bca9" style={{border: 'none'}} allow="accelerometer;gyroscope;autoplay;encrypted-media;picture-in-picture" allowFullScreen={true} className="absolute top-0 left-0 w-full h-full" fetchPriority="high"></iframe>
           </div>
 
           <div className="flex flex-col gap-2 min-[400px]:flex-row justify-center">
