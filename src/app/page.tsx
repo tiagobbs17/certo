bb70f4b812c39b5fce7779daefad445ac54c13d4
Tiago Barbosa <tiagobbs17@gmail.com>
2026-01-23 05:45:16 +0000

    em baixo desse bloco coloque esse bloco ''Lo que vas a aprender ''

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,8 +21,8 @@ export default function Home() {
         <Hero />
         <About />
         <Features />
-        <Benefits />
         <CourseContent />
+        <Benefits />
         <Bonuses />
         <Testimonials />
         <Certificate />
